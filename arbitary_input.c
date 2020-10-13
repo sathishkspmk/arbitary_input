c238542c86852978797f3dacba4740996d2d075d
sathishkspmk <sathishkspmk@gmail.com>
2020-10-13 23:16:37 +0530

    Update arbitary_input.c

--- arbitary_input.c
+++ arbitary_input.c
@@ -30,7 +30,17 @@ int main(){
     else // No match.
         printf("input not found in any of the data types\n");
 
+    FILE *fptr;
+    fptr = fopen("C:\program.txt","w");
 
+    if(fptr == NULL)
+    {
+       printf("Error!");
+       exit(1);
+    }
+
+    fputs(inp_value, fptr); // writing data which ever user is entrering any type of value
+    fclose(fptr);
 return 0;
 }
 
